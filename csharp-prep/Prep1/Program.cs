2b5aaf4ee3f9c3be5354ff07ef1e524bbf8dc88f
BuildTools <unconfigured@null.spigotmc.org>
2024-01-12 18:19:48 -0700

    Finished the prep 1 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
+//Michael Zaugg 1-12-2024
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Get user input
+        System.Console.WriteLine("What is your first name?");
+        string first_name = Console.ReadLine();
+
+        System.Console.WriteLine("What is your last name?");
+        string last_name = Console.ReadLine();
+
+        //Write the user input to the console
+        System.Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+
+        Console.WriteLine("What's your age?");
+
+        var ageString = Console.ReadLine();
+
+        Console.WriteLine($"My age is {ageString}");
+        //or Console.WriteLine("My age is " + age + ".");
+
+
+        //Convert string to int
+        int age = int.Parse(ageString);
+
+        if (age < 18) {
+
+            Console.WriteLine("Ahhhhhhhh");
+
+        }
     }
+
 }
